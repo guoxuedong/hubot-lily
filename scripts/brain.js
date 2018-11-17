d3e73bb9f9a15b4afb63202db1047649a0cf39b2
guoxuedong <guoxuedong@xiaomi.com>
2018-11-17 20:09:03 +0800

    fix up/down

--- scripts/brain.js
+++ scripts/brain.js
@@ -290,17 +290,17 @@ module.exports = (robot) => {
         let taglist = robot.brain.get(TAGS) || []
         let index = taglist.indexOf(tag)
         if (index < 0) {
-            msg.send ('ERR: ${tag} not exist')
+            msg.send (`ERR: ${tag} not exist`)
             return
         } else if (index == 0) {
-            msg.send ('ERR: ${tag} is the first')
+            msg.send (`ERR: ${tag} is the first`)
             return
         }
 
         taglist.splice(index, 1)
         taglist.splice(index-1 , 0, tag)
         robot.brain.set(TAGS, taglist)
-        msg.send ('OK: up ${tag}')
+        msg.send (`OK: up ${tag}`)
     })
 
     robot.respond (/down t:(.+)/i, (msg) => {
@@ -309,17 +309,17 @@ module.exports = (robot) => {
         let taglist = robot.brain.get(TAGS) || []
         let index = taglist.indexOf(tag)
         if (index < 0) {
-            msg.send ('ERR: ${tag} not exist')
+            msg.send (`ERR: ${tag} not exist`)
             return
         } else if (index == taglist.length - 1) {
-            msg.send ('ERR: ${tag} is the last')
+            msg.send (`ERR: ${tag} is the last`)
             return
         }
 
         taglist.splice(index, 1)
         taglist.splice(index+1 , 0, tag)
         robot.brain.set(TAGS, taglist)
-        msg.send ('OK: down ${tag}')
+        msg.send (`OK: down ${tag}`)
     })
 
     robot.respond (/save/i, (msg) => {
