7f280edb77e73aea64a50b7ca47dcdae8093a9d3
guoxuedong <guoxuedong@xiaomi.com>
2018-12-12 11:46:44 +0800

    update

--- scripts/upload2sm.sh
+++ scripts/upload2sm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#/home/guoxuedong/bin/cutycapt/bin/CutyCapt --url=http://aqicn.org/city/beijing/haidianwanliu/ --out=pm25.jpg
+#/bin/cutycapt/bin/CutyCapt --url=http://aqicn.org/city/beijing/haidianwanliu/ --out=pm25.jpg
 #result=$(curl -F "smfile=@pm25.jpg" "https://sm.ms/api/upload")
 echo '{"code":"success","data":{"width":800,"height":7046,"filename":"pm25.jpg","storename":"5bd7fe8f82b8f.jpg","size":663012,"path":"\/2018\/10\/30\/5bd7fe8f82b8f.jpg","hash":"Ur78sCKqz1ulMDY","timestamp":1540882063,"ip":"111.202.192.3","url":"https:\/\/i.loli.net\/2018\/10\/30\/5bd7fe8f82b8f.jpg","delete":"https:\/\/sm.ms\/delete\/Ur78sCKqz1ulMDY"}}'
